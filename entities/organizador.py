b6fdb4977af6f242874aacb05f3b57de62223e62
DudaBouAnni <posercorpsedudinha@gmail.com>
2025-11-27 17:15:40 -0300

    Adicionando mais loops de cadstro

--- entities/organizador.py
+++ entities/organizador.py
@@ -8,53 +8,50 @@ def menu_organizador():
         print("3. Apagar Organizador")
         print("4. Listar todos os Organizadores")
         print("5. Sair do Cadastro")
+        print("\n")
+        op = int(input('Entre com a sua opção:'))
 
-        while True:
-           print("\n")
-           op = int(input('Entre com a sua opção:'))
         #Busca
-           if op == 1:
-               nome= int(input('Entre com o código do Organizador:'))
-               con = conexao()
-               cursor = con.cursor()
-               cursor.execute= "SELECT * FROM Genero where id_organizador = %s"
-               resultados = cursor.fetchall()
-               if not resultados:
-                   print('Organizador', nome, 'não existe')
-               else:
-
-                   for linha in resultados:
-                       print(linha)
+        if op == 1:
+            nome= int(input('Entre com o código do Organizador:'))
+            con = conexao()
+            cursor = con.cursor()
+            cursor.execute("SELECT * FROM Organizador where id_organizador = %s", (nome,))
+            resultados = cursor.fetchall()
+            if not resultados:
+                print('Organizador', nome, 'não existe')
+            else:
+                for linha in resultados:
+                    print(linha)
         #Inserção
-           elif op == 2:
-
-               nome = input('Entre com o nome do Organizador:')
-               comando = "INSERT INTO organizador (nome_organizador) VALUES (%s)"
-               con = conexao()
-               cursor = con.cursor()
-               cursor.execute(comando, (nome,))
-               con.commit()
-               print(f'Organizador', nome, 'inserido com sucesso!')
+        elif op == 2:
+            nome = input('Entre com o nome do Organizador:')
+            comando = "INSERT INTO organizador (nome_organizador) VALUES (%s)"
+            con = conexao()
+            cursor = con.cursor()
+            cursor.execute(comando, (nome,))
+            con.commit()
+            print(f'Organizador', nome, 'inserido com sucesso!')
         #Deleção
-           elif op == 3:
-               nome = int(input('Entre com o código do Organizador:'))
-               con = conexao()
-               cursor = con.cursor()
-               comando = "DELETE FROM organizador where id_organizador = %s" + str(nome)
-               cursor.execute(comando)
-               cursor.commit()
-               print('Gênero', nome, "Deletado com sucesso!")
+        elif op == 3:
+            id_organizador = int(input('Entre com o ID do Organizador:'))
+            con = conexao()
+            cursor = con.cursor()
+            comando = "DELETE FROM organizador where id_organizador = %s"
+            cursor.execute(comando,(id_organizador,))
+            con.commit()
+            print("Organizador deletado com sucesso!")
         #Listagem
-           elif op == 4:
-               con = conexao()
-               cursor = con.cursor()
-               cursor.execute("SELECT id_genero, nome_organizador FROM organizador;")
-               resultados = cursor.fetchall()
-               for linha in resultados:
-                   print(linha)
+        elif op == 4:
+            con = conexao()
+            cursor = con.cursor()
+            cursor.execute("SELECT id_organizador, nome_organizador FROM organizador;")
+            resultados = cursor.fetchall()
+            for linha in resultados:
+                print(linha)
         #Saída
-           elif op == 5:
-               print("Saindo...")
-               break
-           else:
-               print("Opção inválida. Tente novamente.")
+        elif op == 5:
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
